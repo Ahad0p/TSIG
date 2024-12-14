3f4cd7e19455daf584f30c6ec434c6d0f6e58192
Ahad0p <ahadkhan.ai306@gmail.com>
2024-12-14 12:38:25 +0530

    swap two numbers

--- main.java
+++ main.java
@@ -1,5 +1,10 @@
-public class main{
-    public static void main(String[] args) {
-        
+    public class main{
+        public static void main(String[] args) {
+            int a=10;
+            int b=20;
+            a=a+b;
+            b=a-b;
+            a=a-b;
+            System.out.println("after swap a:"+a+"and after swap b:"+b);
+        }
     }
-}
